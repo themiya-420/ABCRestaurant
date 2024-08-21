9b1e45e38e2347e83a829f25896b3542b7b1ab90
themiya-420 <themiya420@gmail.com>
2024-08-21 11:50:53 +0530

    Implemented MYSQL Database Connection

--- src/main/java/com/user/util/DBConnection.java
+++ src/main/java/com/user/util/DBConnection.java
@@ -1,5 +1,26 @@
 package com.user.util;
 
-public class DBConnection {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBConnection extends CommonUtil {
+	
+	 private static Connection connection;
+
+	    private DBConnection() {
+
+	    }
+
+	    public static Connection getConnection() throws ClassNotFoundException, SQLException {
+
+	        if (connection == null || connection.isClosed() ) {
+	            connection = DriverManager.getConnection(properties.getProperty(CommonConstants.Driver_Name));
+	            connection = DriverManager.getConnection(properties.getProperty(CommonConstants.URL),
+	                    properties.getProperty(CommonConstants.UserName),properties.getProperty(CommonConstants.Password));
+	        }
+
+	        return connection;
+	    }
 
 }
