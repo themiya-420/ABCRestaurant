c3a5157c3ac77eb39dad6a15c98a1187eff213e3
themiya-420 <themiya420@gmail.com>
2024-08-21 11:45:02 +0530

    Implemented Common Constants of the project

--- src/main/java/com/user/util/CommonConstants.java
+++ src/main/java/com/user/util/CommonConstants.java
@@ -1,5 +1,48 @@
 package com.user.util;
 
 public class CommonConstants {
+	
+    public static  final  String Query_XML = "queryFilePath";
+
+    public static final String Property_File = "config.properties";
+
+    public static  final String Tag_Name = "query";
+
+    public static final  String Attrib_Name = "id";
+
+    public static final String Comma = ",";
+
+    public static final String URL = "url";
+
+    public static final String UserName = "userName";
+
+    public static final String Password = "password";
+
+    public static final String Driver_Name = "driverName";
+
+    public static final int Column_Index_one = 1;
+
+    public static final int Column_Index_two = 2;
+
+    public static final int Column_Index_three = 3;
+
+    public static final int Column_Index_four= 4;
+
+    public static final int Column_Index_five = 5;
+
+    public static final int Column_Index_six = 6;
+
+    //User constants
+    public static final String user_id_prefix = "ABC00";
+
+    public static final String Create_User_Table = "create_user_table";
+
+    public static final String signup_user = "signup_user";;
+
+    public static final String update_user = "update_user";;
+
+    public static final String delete_user = "delete_user";;
+
+    public static final String get_user_ids = "get_user_ids";;
 
 }
