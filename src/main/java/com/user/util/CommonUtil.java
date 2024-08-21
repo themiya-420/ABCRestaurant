8b99d29e8c46742d6267bb3e774bdd6a9b32b123
themiya-420 <themiya420@gmail.com>
2024-08-21 11:49:26 +0530

    Implemented common utils of the project

--- src/main/java/com/user/util/CommonUtil.java
+++ src/main/java/com/user/util/CommonUtil.java
@@ -1,5 +1,21 @@
 package com.user.util;
 
+import java.io.IOException;
+import java.util.Properties;
+
 public class CommonUtil {
+	
+	public static final Properties properties = new Properties();
+	
+	static {
+		
+		try {
+			
+			properties.load(QueryUtil.class.getResourceAsStream(CommonConstants.Property_File));
+			
+		} catch (IOException e) {
+			System.out.println(e.getMessage());
+		}
+	}
 
 }
