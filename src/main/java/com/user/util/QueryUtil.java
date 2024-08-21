96bb87822262a33e74d95a864aa215002b27d644
themiya-420 <themiya420@gmail.com>
2024-08-21 11:52:14 +0530

    Implemented Utils to run specific queries from a xml

--- src/main/java/com/user/util/QueryUtil.java
+++ src/main/java/com/user/util/QueryUtil.java
@@ -1,5 +1,36 @@
 package com.user.util;
 
-public class QueryUtil {
+import java.io.File;
+import java.io.IOException;
 
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
+public class QueryUtil extends CommonUtil {
+	
+	public static String QueryById(String id) throws SAXException, IOException, ParserConfigurationException{
+
+        NodeList nodeList;
+        Element element = null;
+
+        nodeList = DocumentBuilderFactory.newInstance()
+                .newDocumentBuilder().parse(new File(System.getProperty("catalina.base") + "wtwebapps\\ABCRestaurant\\WEB-INF\\queries.xml"))
+                .getElementsByTagName(CommonConstants.Tag_Name);
+
+        for (int i = 0; i < nodeList.getLength(); i++) {
+            element = (Element) nodeList.item(i);
+
+            if (element.getAttribute(CommonConstants.Attrib_Name).equals(id)) {
+                break;
+            }
+        }
+
+        return element.getTextContent().trim();
+
+    }
+	
 }
