873a9801a7e9128bb6a7395af92842b8793cfc77
themiya-420 <themiya420@gmail.com>
2024-08-22 11:26:52 +0530

    Implemented user Model

---
+++ src/main/java/com/user/model/User.java
@@ -0,0 +1,50 @@
+package com.user.model;
+
+public class User {
+	
+	private String id;
+    private String username;
+    private String password;
+    private String email;
+    private String phone;
+    private String role;
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPhone() {
+		return phone;
+	}
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	public String getRole() {
+		return role;
+	}
+	public void setRole(String role) {
+		this.role = role;
+	}
+    
+    
+
+}
