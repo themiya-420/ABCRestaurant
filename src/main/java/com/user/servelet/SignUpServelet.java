2de5c08b83c2360a370592d60542c71fdd1dfc5a
themiya-420 <themiya420@gmail.com>
2024-08-22 11:30:10 +0530

    Implemented Servelet for SignUp

---
+++ src/main/java/com/user/servelet/SignUpServelet.java
@@ -0,0 +1,64 @@
+package com.user.servelet;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+
+import com.user.model.User;
+import com.user.service.UserServiceImpl;
+import com.user.service.IUserService;
+
+/**
+ * Servlet implementation class SignUpServelet
+ */
+@WebServlet("/signup")
+public class SignUpServelet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public SignUpServelet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		
+		response.setContentType("text/html");
+		
+		User user = new User();
+		
+        user.setUsername(request.getParameter("username"));
+        user.setPassword(request.getParameter("password"));
+        user.setEmail(request.getParameter("email"));
+        user.setPhone(request.getParameter("phone"));
+        user.setRole(request.getParameter("role"));
+        
+        IUserService iUserService = new UserServiceImpl();
+        iUserService.userSignUp(user);
+        
+        request.setAttribute("User", user);
+        jakarta.servlet.RequestDispatcher requestDispatcher = getServletContext().getRequestDispatcher("/login.jsp");
+        requestDispatcher.forward(request, response);
+     
+	}
+
+}
